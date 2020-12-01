c61bbbf8a6d9dfa0a8f36560bdafd02d09aec64b
MD FIROJ UDDIN <48468312+FirojUddinA@users.noreply.github.com>
2020-12-01 14:13:22 +0600

    git
    git

---
+++ js/script.js
@@ -0,0 +1,8 @@
+$(document).ready(function(){
+    $("#banner-heading").mouseenter(function(){
+        $(this).text("digital.");
+    }).mouseleave(function(){
+        $(this).text("Sportlight.");
+  
+    })
+});
